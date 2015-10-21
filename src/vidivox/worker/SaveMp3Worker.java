a573df4bf27b848886eb801d969ae6e71d4ef7c5
Jacky Mai <jacky.jy.mai@gmail.com>
2015-10-21 20:44:06 +1300

    Simplified the mp3 saving logic. Will not longer create a temporary wav file.

--- src/vidivox/worker/SaveMp3Worker.java
+++ src/vidivox/worker/SaveMp3Worker.java
@@ -1,8 +1,5 @@
 package vidivox.worker;
 import java.io.File;
-import java.io.IOException;
-
-import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 import javax.swing.SwingWorker;
 
@@ -19,52 +16,26 @@ import vidivox.VidivoxPlayer;
 public class SaveMp3Worker extends SwingWorker<Void, Void> {
 	private String outputName;
 	private String message;
-	private String parentDir;
 	private VidivoxPlayer vp;
-	private JDialog jd;
-	private String tempName = "vidiTemp_JH.wav";
 	
 	public SaveMp3Worker(String message, File desiredName, VidivoxPlayer vp) {
 		this.message = message;
 		this.outputName = desiredName.getAbsolutePath();
-		this.parentDir = desiredName.getParent();
 		this.vp = vp;
 	}
 	
 	@Override
 	protected Void doInBackground() throws Exception {
-		JOptionPane jop = new JOptionPane("Saving message to mp3 file as requested...", 
-				JOptionPane.INFORMATION_MESSAGE);
-		jd = jop.createDialog(vp.getPlayerComponent(), "Saving operation");
-		jd.setModal(false);
-		jd.setVisible(true);
-		
-		String save = "echo '" + message + "' | text2wave -o '" + parentDir + File.separator + tempName + "';";
-		String convert = "ffmpeg -i '" + parentDir + File.separator + tempName + "' '" + outputName + "'";
+		String save = "echo '" + message + "' | text2wave | ffmpeg -i - '" + outputName + "'";
 
-		ProcessBuilder builder = new ProcessBuilder("/bin/bash", "-c", save + convert);
-		
-		Process process = builder.start();
-		process.waitFor();
+		ProcessBuilder builder = new ProcessBuilder("/bin/bash", "-c", save);
+		builder.start();
 		
 		return null;
 	}
 	
 	@Override
 	protected void done() {
-		String cmd = "rm \"" + parentDir + File.separator + tempName + "\"";
-
-		ProcessBuilder builder = new ProcessBuilder("/bin/bash", "-c", cmd);
-	
-		try {
-			Process process = builder.start();
-			process.waitFor();
-		} catch (IOException | InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-		jd.setVisible(false);
-		
 		JOptionPane.showMessageDialog(vp.getPlayerComponent(), "Successfully saved as '" + outputName + "'");
 	}
 }
