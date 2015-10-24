5335e68171138d88e6b67516884904ef00b1f726
Jacky Mai <jacky.jy.mai@gmail.com>
2015-10-25 03:12:21 +1300

    Added icons for the audio control JButtons

--- src/vidivox/gui/AudioControlPanel.java
+++ src/vidivox/gui/AudioControlPanel.java
@@ -13,39 +13,48 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import vidivox.filechooser.FileOpener;
-import vidivox.filechooser.FileSaver;
 import vidivox.model.AudioTrack;
 
 public class AudioControlPanel extends JPanel {
-	private JButton exportButton;
-	
-	public AudioControlPanel(final JFrame mainFrame, final StatusPanel statusPanel, final AudioScrollPanel audioScrollPanel) {
+	private static final long serialVersionUID = 1L;
+
+	public AudioControlPanel(final JFrame mainFrame, final StatusPanel statusPanel, 
+			final AudioScrollPanel audioScrollPanel, final PlayerControlPanel playerControlPanel) {
 		this.setLayout(new GridBagLayout());
 		
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.gridx = 0;
 		gbc.gridy = GridBagConstraints.RELATIVE;
 		
-		JButton audioAddButton = new JButton("Add");
+		JButton audioAddButton = new JButton(new ImageIcon("src" + File.separator + "icons" + File.separator + "add.png"));
+		removeButtonStyle(audioAddButton);
 		audioAddButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 			if(FileOpener.openAudio(mainFrame)) {
-				statusPanel.setChosenAudioLabel("Audio track: " + VidivoxGUI.vm.getChosenAudio().getName());
+				statusPanel.setChosenAudioLabel("Audio Tracks: " + (VidivoxGUI.vm.getAudioListSize()+1));
 				
 				AudioTrack newTrack = new AudioTrack(VidivoxGUI.vm.getChosenAudio(), (int)VidivoxGUI.vp.getCurrentTime());
 				audioScrollPanel.addAudioTrack(newTrack);
 				VidivoxGUI.vm.addAudioTrack(newTrack);
 				
-				enableExportButton();
+				playerControlPanel.enableExportButton();
 			}
 		}});
 		this.add(audioAddButton, gbc);
 		
-		JButton audioEditButton = new JButton("Edit");
+		JButton audioEditButton = new JButton(new ImageIcon("src" + File.separator + "icons" + File.separator + "edit.png"));
+		removeButtonStyle(audioEditButton);
+		audioEditButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
 		this.add(audioEditButton, gbc);
 		
-		JButton audioDeleteButton = new JButton("Delete");
+		JButton audioDeleteButton = new JButton(new ImageIcon("src" + File.separator + "icons" + File.separator + "delete.png"));
+		removeButtonStyle(audioDeleteButton);
 		audioDeleteButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -56,22 +65,16 @@ public class AudioControlPanel extends JPanel {
 			}
 		});
 		this.add(audioDeleteButton, gbc);
-		
-		exportButton = new JButton("Export");
-//		exportButton.setMargin(new Insets(2,6,2,6));
-		exportButton.setEnabled(false);
-		exportButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				FileSaver.exportFile(mainFrame, "video", null);
-			}
-		});
-		this.add(exportButton, gbc);
 	}
 	
-	protected void enableExportButton() {
-		if(VidivoxGUI.vm.getChosenVideo() != null && VidivoxGUI.vm.getChosenAudio() != null) {
-			exportButton.setEnabled(true);
-		}
+	protected void removeButtonStyle(JButton button) {
+		button.setMargin(new Insets(0, 0, 0, 0));
+		button.setBorderPainted(false);
+		button.setContentAreaFilled(false);
+		button.setOpaque(false);
+	}
+	
+	protected void audioEditWindow() {
+		
 	}
 }
