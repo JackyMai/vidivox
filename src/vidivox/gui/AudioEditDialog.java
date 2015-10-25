de9163448aafebaade1a835e03fb6ca0c7ea3118
Jacky Mai <jacky.jy.mai@gmail.com>
2015-10-25 23:01:07 +1300

    Added comments for this class

--- src/vidivox/gui/AudioEditDialog.java
+++ src/vidivox/gui/AudioEditDialog.java
@@ -14,40 +14,49 @@ import javax.swing.JTextField;
 
 import vidivox.helper.GenericHelper;
 
+/**
+ * This class will create a customized JOptionPane which checks whether the new insert time entered
+ * by the user is valid or not.
+ * Reference: http://stackoverflow.com/a/13055405
+ * 
+ * Author: Jacky Mai - jmai871
+ * Partner: Helen Zhao - hzha587
+ */
 public class AudioEditDialog extends JDialog implements ActionListener, PropertyChangeListener {
 	private JOptionPane editOptionPane;
 	private AudioScrollPanel audioScrollPanel;
 	private int[] selectedRows;
-	private JTextField editTextField;
+	private JTextField editTextField = new JTextField();
 	private String newTime;
-	private String cancelButton = "Cancel";
 	private String okButton = "OK";
+	private String cancelButton = "Cancel";
 	
 	public AudioEditDialog(JFrame mainFrame, AudioScrollPanel asp) {
+		// Storing the input arguments as global variables
 		this.audioScrollPanel = asp;
 		this.selectedRows = audioScrollPanel.getSelectedRows();
-		
-		this.setLocationRelativeTo(mainFrame);
-		this.setModal(true);
-		
+
+		// Setting up the instructions to help the user to enter the correct insert time
 		String message = "Please enter a new insert time for the audio tracks";
-		String instruction = "Enter in the (HH:)MM:SS format: ";
-		
-		editTextField = new JTextField();
+		String instruction = "Enter in the (HH:)MM:SS format (see time label): ";
 		
 		Object[] structure = {message, instruction, editTextField};
-		Object[] options = {cancelButton, okButton};
+		Object[] options = {okButton, cancelButton};
 		
+		// Setting up the JOptionPane which will display the labels, textfield and buttons
+		// in the correct order
 		editOptionPane = new JOptionPane(structure,
 				JOptionPane.QUESTION_MESSAGE,
 				JOptionPane.OK_CANCEL_OPTION,
 				null,
 				options,
-				options[1]);
+				options[0]);
 		
+		// Some minor settings for the JOptionPane
 		this.setContentPane(editOptionPane);
+		this.setTitle("Edit Insert Time");
+		this.setModal(true);
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		
 		this.addComponentListener(new ComponentAdapter() {
 			@Override
 			public void componentShown(ComponentEvent e) {
@@ -55,15 +64,27 @@ public class AudioEditDialog extends JDialog implements ActionListener, Property
 			}
 		});
 		
+		// Adding the necessary listeners for the textfield and JOptionPane
 		editTextField.addActionListener(this);
 		editOptionPane.addPropertyChangeListener(this);
+		
+		// Packing and setting the relative location of the window to make
+		// sure the JOptionPane will display properly
+		pack();
+		this.setLocationRelativeTo(mainFrame);
 	}
-
+	
+	/**
+	 * This method handles events for the editTextField.
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		editOptionPane.setValue(okButton);
 	}
 	
+	/**
+	 * This methods will respond to the state changes in the JOptionPane.
+	 */
 	@Override
 	public void propertyChange(PropertyChangeEvent e) {
 		String property = e.getPropertyName();
@@ -75,30 +96,40 @@ public class AudioEditDialog extends JDialog implements ActionListener, Property
 			
 			Object value = editOptionPane.getValue();
 			if(value == JOptionPane.UNINITIALIZED_VALUE) {
+				// Ignore because alue is still uninitialized.
 				return;
 			}
 			
+			// Resetting the value of JOptionPane.
+			// Otherwise nothing will happen if the user presses the same button more than once.
 			editOptionPane.setValue(JOptionPane.UNINITIALIZED_VALUE);
 			
 			if(okButton.equals(value)) {
 				newTime = editTextField.getText();
 				
+				// If the new insert time entered by the user is valid,
+				// then loop through all of the selected rows in the table and change
+				// the insert time in the AudioTrack object to the new time.
 				if(GenericHelper.checkValidInsertTime(newTime)) {
 					for(int selected : selectedRows) {
 						audioScrollPanel.setModelValue(newTime, selected, 1);
 					}
+					
 					dispose();
 				} else {
+					// Display an error message telling the user that the new insert time is invalid.
 					JOptionPane.showMessageDialog(this, 
 							"Sorry but \"" + newTime + "\" is not a valid insert time!\n"
-							+ "Please enter according to the (HH:)MM:SS format and make"
-							+ "the new time is between 00:00 and video length.",
+							+ "Please enter a valid time between 00:00 up to " + VidivoxGUI.vp.getVideoLength() +".\n"
+							+ "The (HH:)MM:SS format is hinted next to the video slider.",
 							"OK",
 							JOptionPane.ERROR_MESSAGE);
+					
 					newTime = null;
 					editTextField.requestFocusInWindow();
 				}
 			} else {
+				// The user either closed the dialog or pressed the cancel button.
 				newTime = null;
 				dispose();
 			}
